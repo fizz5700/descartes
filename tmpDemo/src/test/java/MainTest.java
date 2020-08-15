9efeacb468519a280652b7c83f6c36fdd4bb6579
fizz5700 <57882359+fizz5700@users.noreply.github.com>
2020-08-15 17:05:32 +0800

    Update MainTest.java

--- tmpDemo/src/test/java/MainTest.java
+++ tmpDemo/src/test/java/MainTest.java
@@ -1,11 +1,47 @@
 import org.junit.Test;
 
+import java.util.List;
+
 public class MainTest {
     @Test
     public void test(){
-        OutPut outPut=new OutPut();
-        int numbers[]={2,3,4,5};
-        System.out.println( outPut.init(numbers));
+        EnterText enterText=new EnterText();
+        int numbers1[]={2};
+        int numbers2[]={2,3};
+        int numbers3[]={2,3,4};
+        int numbers4[]={2,3,4,5,6,7,8,9};
+        List<List<String>> content1 = enterText.getContent(numbers1);
+        List<List<String>> content2 = enterText.getContent(numbers2);
+        List<List<String>> content3 = enterText.getContent(numbers3);
+        List<List<String>> content4 = enterText.getContent(numbers4);
+        for (int i = 0; i < content1.size(); i++) {
+            for (int j = 0; j < content1.get(i).size(); j++) {
+                System.out.print(content1.get(i).get(j));
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+        for (int i = 0; i < content2.size(); i++) {
+            for (int j = 0; j < content2.get(i).size(); j++) {
+                System.out.print(content2.get(i).get(j));
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+        for (int i = 0; i < content3.size(); i++) {
+            for (int j = 0; j < content3.get(i).size(); j++) {
+                System.out.print(content3.get(i).get(j));
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+        for (int i = 0; i < content4.size(); i++) {
+            for (int j = 0; j < content4.get(i).size(); j++) {
+                System.out.print(content4.get(i).get(j));
+            }
+            System.out.print(" ");
+        }
+       // System.out.println( enterText.getContent(numbers));
     }
 
 }
